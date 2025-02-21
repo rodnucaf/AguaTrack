79306920758d73ec3972b16f87c4ab0ef630f578
unknown <facundoxrabago@gmail.com>
2025-02-21 00:05:33 -0300

    creada carpeta Models, creada clase modelo Agua

---
+++ Models/Agua.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AguaTrack.Models
+{
+    public class Agua
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+}
