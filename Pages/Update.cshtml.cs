6dfed98483ace9586a0bae74e6692a55b24d0107
unknown <facundoxrabago@gmail.com>
2025-02-21 16:27:46 -0300

    creada página Update

---
+++ Pages/Update.cshtml.cs
@@ -0,0 +1,75 @@
+using System.Globalization;
+using AguaTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+
+namespace AguaTrack.Pages
+{
+    public class UpdateModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        [BindProperty]
+        public AguaModel Agua { get; set; }
+
+        public UpdateModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Agua = GetById(id);
+
+            return Page();
+        }
+
+        private AguaModel GetById(int id)
+        {
+            var aguaRegistro = new AguaModel();
+
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"SELECT * FROM drinking_water WHERE Id = {id}";
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    aguaRegistro.Id = reader.GetInt32(0);
+                    aguaRegistro.Fecha = DateTime.Parse(reader.GetString(1), CultureInfo.CurrentUICulture.DateTimeFormat);
+                    aguaRegistro.Cantidad = reader.GetInt32(2);
+                }
+
+                return aguaRegistro;
+
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                   $"UPDATE drinking_water SET date ='{Agua.Fecha}', quantity = {Agua.Fecha} WHERE Id = {Agua.Id}";
+
+                tableCmd.ExecuteNonQuery();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
