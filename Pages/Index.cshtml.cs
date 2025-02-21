8ba657f1c9984c5ba998234edfeb726533a3cc60
unknown <facundoxrabago@gmail.com>
2025-02-21 15:22:35 -0300

    agregadas configuraciones, método getAll

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,53 @@
+using AguaTrack.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+using System.Data.SqlTypes;
+using System.Globalization;
 
 namespace AguaTrack.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<AguaModel> Registro { get; set; }
+
+        public IndexModel(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            GetAllRegistros();
+        }
+
+        private List<AguaModel> GetAllRegistros()
+        {
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = $"Select * FROM drinking_water";
+
+                var tableData = new List<AguaModel>();
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tableData.Add
+                    (
+                        new AguaModel
+                        {
+                            Id = reader.GetInt32(0),
+                            Fecha = DateTime.Parse(reader.GetString(1), CultureInfo.CurrentCulture.DateTimeFormat),
+                            Cantidad = reader.GetInt32(2)
+                        }
+                    );
+                }
+                return tableData;
+            }
 
         }
     }
