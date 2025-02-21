70318380ad6337a5a8910c0255b7a0cca2e8e2f3
unknown <facundoxrabago@gmail.com>
2025-02-21 00:06:02 -0300

    cambiado nombre de archivo

---
+++ Models/AguaModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AguaTrack.Models
+{
+    public class AguaModel
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+}
