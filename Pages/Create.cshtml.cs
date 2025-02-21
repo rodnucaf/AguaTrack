c2a845e1b6a83d0016fb45eb21d5214e0c908695
unknown <facundoxrabago@gmail.com>
2025-02-21 00:34:21 -0300

    agregada interfaz IConfiguration, método OnGet

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AguaTrack.Models;
+using Microsoft.Data.Sqlite;
 
 namespace AguaTrack.Pages
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+        public CreateModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+
         }
+
+        public IActionResult OnGet()
+        {
+            if (!ModelState.IsValid) 
+            {
+                return Page();
+            }
+
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = $"INSERT INTO drinking_water(date, quantity) VALUES ('{Agua.Fecha}', {Agua.Cantidad})  ";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            return RedirectToPage("./Index");
+        }
+
+        [BindProperty]
+        public AguaModel Agua { get; set; }
+
+
     }
 }
