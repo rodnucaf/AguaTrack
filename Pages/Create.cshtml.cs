f5b4cf5ea46122612cf5360294aa1b808725093f
unknown <facundoxrabago@gmail.com>
2025-02-20 23:07:26 -0300

    agregado archivo

---
+++ Pages/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AguaTrack.Pages
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
