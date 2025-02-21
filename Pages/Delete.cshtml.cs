1508c2f255c78c1a8bc0897fda12ac387c17c13a
unknown <facundoxrabago@gmail.com>
2025-02-21 16:21:56 -0300

    creada página Delete, creado método Delete

--- Pages/Delete.cshtml.cs
+++ Pages/Delete.cshtml.cs
@@ -1,12 +1,69 @@
+using System.Globalization;
+using AguaTrack.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
 
 namespace AguaTrack.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration _configuration;
+
+        [BindProperty]
+        public AguaModel Agua { get; set; }
+
+        public DeleteModel(IConfiguration configuration)
         {
+            _configuration = configuration;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Agua = GetById(id);
+
+            return Page();
+        }
+
+        private AguaModel GetById(int id)
+        {
+            var aguaRegistro = new AguaModel();
+
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"SELECT * FROM drinking_water WHERE Id = {id}";
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    aguaRegistro.Id = reader.GetInt32(0);
+                    aguaRegistro.Fecha = DateTime.Parse(reader.GetString(1), CultureInfo.CurrentUICulture.DateTimeFormat);
+                    aguaRegistro.Cantidad = reader.GetInt32(2);
+                }
+
+                return aguaRegistro;
+
+            }
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            using (var connection = new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = $"DELETE from drinking_water WHERE Id = {id}";
+
+                tableCmd.ExecuteNonQuery();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
