9db257c282ffa11b533d1dbefd7b58d7de0e1a3f
unknown <facundoxrabago@gmail.com>
2025-02-22 02:39:56 -0300

    agregadas data anotations para validaciones de formato

--- Models/AguaModel.cs
+++ Models/AguaModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace AguaTrack.Models
 {
     public class AguaModel
     {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yy}", ApplyFormatInEditMode = true)]
         public DateTime Fecha { get; set; }
+        [Range(0, Int32.MaxValue, ErrorMessage = "El valor para {0} debe ser POSITIVO.")]
         public int Cantidad { get; set; }
 
     }
